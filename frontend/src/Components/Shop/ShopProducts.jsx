bf5200bb4f503581c673b7e6d8d0acd8b79a475f
smitesh <maniyasmiteshm@gmail.com>
2022-04-11 15:44:28 +0530

    get data

--- frontend/src/Components/Shop/ShopProducts.jsx
+++ frontend/src/Components/Shop/ShopProducts.jsx
@@ -14,7 +14,7 @@ export default function ShopProducts() {
 
     const getProduct = async () => {
         console.log('hello')
-        const shop = await axios.get(`${API}/api/productDetail/shop/62511363e7f67e1b640d0b12`)
+        const shop = await axios.get(`${API}/api/productDetail/shop/6253f02125e5f259fc698b1b`)
         setallProduct(shop.data.userdata)
         console.log(shop.data.userdata)
     }
@@ -33,7 +33,7 @@ export default function ShopProducts() {
                             description={val.description}
                             price={val.price}
                             stock={val.stock}
-                        // image={val.image}
+                            imgId={val.image.imgId}
                         />
                     ))}
                 </Flex>
